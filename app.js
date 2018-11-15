851c618add053e5a9408a493e7183483d5c9d6ff
Vishesh <visheshsingh16@gmail.com>
2018-11-14 19:24:26 -0500

    Displaying the profile

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // create an instance of github
 const github = new Github();
+// init user
+const ui = new UI();
+
 const searchUser = document.getElementById("searchUser");
 
-searchUser.addEventListener("keydown", function(e) {
+searchUser.addEventListener("keypress", function(e) {
   const userText = e.target.value;
   if (userText !== "") {
-    github.getUser(userText).then(data => console.log(data));
+    github.getUser(userText).then(data => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    });
+  } else {
+    // clear profile
   }
 });
